3f080b3af1e4303250033e02bb6c1bf067863b29
mauricio7712 <mauricioduraes37@gmail.com>
2025-04-21 21:05:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let botão = document.querySelector("#botão");
+       botão.addEventListener ("click", trocatexto);
+        
+       function trocatexto(){
+              botão.innerHTML="seja bem vindo , aqui tem um pouco sobre minha vida";
+       }
